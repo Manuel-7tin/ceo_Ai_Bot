9f68ba6fbfa00026a4143e32d01886c26adfc925
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-05-12 23:29:26 +0100

    just work already again 2

--- temp.py
+++ temp.py
@@ -27,6 +27,7 @@ with st.echo():
         )
 
     options = Options()
+    options.add_argument("--keep-alive")
     # options.add_argument("--disable-gpu")
     # options.add_argument("--headless")
 
